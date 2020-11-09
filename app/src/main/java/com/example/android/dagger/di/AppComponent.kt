7b5986ceaac94019c5b16dad37543bcba73d1e79
Javier <javilena87@gmail.com>
2020-11-09 17:44:59 +0100

    @Module, @Binds and @BindsInstance annotations

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,10 +1,17 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
     // Classes that can be injected by this Component
     fun inject(activity: RegistrationActivity)
 }

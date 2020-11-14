09bea143fe9026082cb64a2ad0ba82fef0b9be62
Javier <javilena87@gmail.com>
2020-11-14 17:32:56 +0100

    Refactoring the Login Flow

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationComponent
@@ -22,4 +23,5 @@ interface AppComponent {
     fun inject(activity: MainActivity)
 
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 }
---
+++ app/src/main/java/com/example/android/dagger/login/LoginComponent.kt
@@ -0,0 +1,19 @@
+package com.example.android.dagger.login
+
+import com.example.android.dagger.di.ActivityScope
+import dagger.Subcomponent
+
+// Scope annotation that the LoginComponent uses
+// Classes annotated with @ActivityScope will have a unique instance in this Component
+@ActivityScope
+// Definition of a Dagger subcomponent
+@Subcomponent
+interface LoginComponent {
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): LoginComponent
+    }
+
+    fun inject(activity: LoginActivity)
+}
